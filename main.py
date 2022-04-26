cdea26c2d721f0dcaaf34942daf2e5dde472dc1a
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2022-04-26 14:50:28 +0700

    Added some function

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import json
-gen = input('Enter something: ')
-gen = 1
-while gen <= 7:
-	data = json.load(open('resource/gen-'+ str(gen) +'.json'))
-	#print(data)
-	#Pokemon, Number, Type1, Type2, Ability1, Ability2, Ability3, HP, Attack, Defense, Sp. Atk, Sp. Def, Speed, Sprite
+data = json.load(open('resource/all-gen.json'))
+#print(data)
+#Name, ID, Type1, Type2, Ability1, Ability2, Ability3, HP, Attack, Defense, Sp. Atk, Sp. Def, Speed, Sprite
+genIdList = [(1,151),(152,251),(252,386),(387,493),(494,649),(650,721),(722,807)]
+#for i in data:
+#	print(i['Pokemon'])
+def searchByName(inString):
+	idList = []
 	for i in data:
-		print(i['Pokemon'])
-	gen = gen + 1
+		if inString in i['pokemon']:
+			idList.append(i['ID'])
+	return idList
+
+def getByID(inId):
+	chosenOne = data[inId-1]
+	info = ""
+	for i in chosenOne:
+		info = info + "%10s :  %s  \n"%(i,str(chosenOne[i]))
+	#print(info)
+	return info
+def getGen(gen):
+	#print(genIdList[gen-1])
+	startId,endId = genIdList[gen-1]
+	for i in range(startId,endId+1):
+		print(getByID(i))
+hehe = searchByName(input("Enter to search: "))
+for i in hehe:
+	print(getByID(i))

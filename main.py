2c722c143871535f7e94eed0820c789fb2c81d46
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2022-04-26 12:33:48 +0700

    a

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import json
+gen = input('Enter something: ')
+gen = 1
+while gen <= 7:
+	data = json.load(open('resource/gen-'+ str(gen) +'.json'))
+	#print(data)
+	#Pokemon, Number, Type1, Type2, Ability1, Ability2, Ability3, HP, Attack, Defense, Sp. Atk, Sp. Def, Speed, Sprite
+	for i in data:
+		print(i['Pokemon'])
+	gen = gen + 1

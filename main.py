12ede770cb1a0103368dbc84cff4d0f3d09b9652
CrazyMeowl <62349601+CrazyMeowl@users.noreply.github.com>
2022-05-05 17:59:31 +0700

    Added some handler code for move

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import json
-data = json.load(open('resource/all-gen.json'))
-#print(data)
+pokemonData = json.load(open('resource/all-gen.json'))
+moveData = json.load(open('resource/moves.json'))
+#print(pokemonData)
 #Name, ID, Type1, Type2, Ability1, Ability2, Ability3, HP, Attack, Defense, Sp. Atk, Sp. Def, Speed, Sprite
 genIdList = [(1,151),(152,251),(252,386),(387,493),(494,649),(650,721),(722,807)]
-#for i in data:
+#for i in pokemonData:
 #	print(i['Pokemon'])
 # Return a list of pokemon that match the search string
-def searchByName(inString):
+def searchPokeByName(inString):
 	idList = []
-	for i in data:
+	for i in pokemonData:
 		if inString.lower() in i['pokemon'].lower():
 			idList.append(i['ID'])
 	return idList
 #get pokemon name by id return info string
-def getByID(inId):
-	chosenOne = data[inId-1]
+def getPokeByID(inId):
+	chosenOne = pokemonData[inId-1]
 	info = ""
 	for i in chosenOne:
 		info = info + "%10s :  %s  \n"%(i,str(chosenOne[i]))
@@ -26,6 +27,18 @@ def getGen(gen):
 	startId,endId = genIdList[gen-1]
 	for i in range(startId,endId+1):
 		print(getByID(i))
+
+def getMoves():
+	for i in moveData:
+		for a in i:
+			print(a,":",i[a],end="|")
+		print()
+def getMoveByName(inString):
+	moveList = []
+	for i in moveData:
+		if inString.lower() in i['Name'].lower():
+			moveList.append(i['Name'])
+	return moveList
+
 while True:
-	for i in searchByName(input("Enter to search: ")):
-		print(getByID(i))
+	print(getMoveByName(input("Enter a move name: ")))

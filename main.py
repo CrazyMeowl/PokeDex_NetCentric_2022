3cb998531c73bc3eebcc2431cf8c544b79691c24
CrazyMeowl <62349601+CrazyMeowl@users.noreply.github.com>
2022-05-08 15:45:02 +0700

    Fixed main file

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 import json
-<<<<<<< Updated upstream
-pokemonData = json.load(open('resource/all-gen.json'))
-moveData = json.load(open('resource/moves.json'))
-#print(pokemonData)
-=======
-<<<<<<< HEAD
 import os
 data = json.load(open('resource/all-gen.json'))
-#print(data)
-=======
+
 pokemonData = json.load(open('resource/all-gen.json'))
 moveData = json.load(open('resource/moves.json'))
-#print(pokemonData)
->>>>>>> d80d6894fefd36d6853d29823525028d976ad2bd
->>>>>>> Stashed changes
+typeList = ['NORMAL','FIGHTING','FLYING','POISON','GROUND','ROCK','BUG','GHOST','STEEL','FIRE','WATER','GRASS','ELECTRIC','PSYCHIC','ICE','DRAGON','DARK','FAIRY'] 
 #Name, ID, Type1, Type2, Ability1, Ability2, Ability3, HP, Attack, Defense, Sp. Atk, Sp. Def, Speed, Sprite
 genIdList = [(1,151),(152,251),(252,386),(387,493),(494,649),(650,721),(722,807)]
 #for i in pokemonData:
 #	print(i['Pokemon'])
 # Return a list of pokemon that match the search string
-<<<<<<< Updated upstream
+
+
 def searchPokeByName(inString):
-=======
-<<<<<<< HEAD
-#typeList = ['NORMAL','FIRE','WATER','ELECTRIC','GRASS','ICE','FIGHTING','POISON','GROUNG','FLYING','PSYCHIC','BUG','ROCK','GHOST','DRAGON','DARK','STEEL','FAIRY']
-typeList = ['NORMAL','FIGHTING','FLYING','POISON','GROUND','ROCK','BUG','GHOST','STEEL','FIRE','WATER','GRASS','ELECTRIC','PSYCHIC','ICE','DRAGON','DARK','FAIRY'] 
-def searchByName(inString):
-=======
-def searchPokeByName(inString):
->>>>>>> d80d6894fefd36d6853d29823525028d976ad2bd
->>>>>>> Stashed changes
 	idList = []
 	for i in pokemonData:
 		if inString.lower() in i['pokemon'].lower():
@@ -43,12 +26,11 @@ def getPokeByID(inId):
 		info = info + "%10s :  %s  \n"%(i,str(chosenOne[i]))
 	#print(info)
 	return info
-# Print all pokemon in that gen
+
 def getGen(gen):
-	#print(genIdList[gen-1])
+
 	startId,endId = genIdList[gen-1]
 	for i in range(startId,endId+1):
-<<<<<<< Updated upstream
 		print(getPokeByID(i))
 
 def getMoves():
@@ -56,6 +38,10 @@ def getMoves():
 		for a in i:
 			print(a,":",i[a],end="|")
 		print()
+def getMove(moveName):
+	for move in moveData:
+		if inString.lower() in move['Name'].lower():
+			return move
 def getMoveByName(inString):
 	moveList = []
 	for i in moveData:
@@ -63,11 +49,9 @@ def getMoveByName(inString):
 			moveList.append(i['Name'])
 	return moveList
 
-while True:
-	print(getMoveByName(input("Enter a move name: ")))
-=======
-<<<<<<< HEAD
-		print(getByID(i))
+def dmgCalculator(inMove,pokemon):
+		move = getMove(Move)
+
 '''
 while True:
 	for i in searchByName(input("Enter to search: ")):
@@ -102,24 +86,5 @@ while row < 18:
 	while col < 18:	
 		print(f'[{typeList[row]}][{typeList[col]}]: {typeChart[row][col]}')
 		col = col + 1
-	row = row + 1
-#print(typeChart)
-=======
-		print(getPokeByID(i))
+	row = row + 1 
 
-def getMoves():
-	for i in moveData:
-		for a in i:
-			print(a,":",i[a],end="|")
-		print()
-def getMoveByName(inString):
-	moveList = []
-	for i in moveData:
-		if inString.lower() in i['Name'].lower():
-			moveList.append(i['Name'])
-	return moveList
-
-while True:
-	print(getMoveByName(input("Enter a move name: ")))
->>>>>>> d80d6894fefd36d6853d29823525028d976ad2bd
->>>>>>> Stashed changes
